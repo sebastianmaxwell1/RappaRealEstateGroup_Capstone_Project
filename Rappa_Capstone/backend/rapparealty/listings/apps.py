1a8e07f7e0fbb5d92b28c38bb85887118a1b0e68
Sebastian <maxwellsebas@gmail.com>
2021-05-29 14:37:03 -0400

    Created Listings App, works after troubleshooting

---
+++ Rappa_Capstone/backend/rapparealty/listings/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class ListingsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'listings'
+
+
+
+
+
+# class ListingsConfig(AppConfig):
+#     name = 'listings'
--- Rappa_Capstone/backend/rapparealty/rapparealty/settings.py
+++ Rappa_Capstone/backend/rapparealty/rapparealty/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'realtors',
+    'listings'
 
 
 ]
