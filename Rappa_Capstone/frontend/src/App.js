30487a30977a6c4c511e075a39790496382a4a45
Sebastian <maxwellsebas@gmail.com>
2021-05-30 13:59:43 -0400

    Set up post request for sign in and sign up

--- Rappa_Capstone/frontend/src/App.js
+++ Rappa_Capstone/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import About from './containers/About';
 import Contact from './containers/Contact';
@@ -10,25 +10,29 @@ import SignUp from './containers/SignUp';
 import NotFound from './components/NotFound';
 import Layout from './hocs/Layout';
 
-import './sass/main.scss'
 
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './sass/main.scss';
 
 const App = () => (
-    <Router>
-        <Layout>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/contact' component={Contact} />
-                <Route exact path='/listings' component={Listings} />
-                <Route exact path='/listings/:id' component={ListingDetail} />
-                <Route exact path='/login' component={Login} />
-                <Route exacct path='/signup' component={SignUp} />
-                <Route component={NotFound} />
-            </Switch>
-        </Layout>
-    </Router>
-
-); 
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/contact' component={Contact} />
+                    <Route exact path='/listings' component={Listings} />
+                    <Route exact path='/listings/:id' component={ListingDetail} />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/signup' component={SignUp} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Layout>
+        </Router>
+    </Provider>
+);
 
 export default App;
