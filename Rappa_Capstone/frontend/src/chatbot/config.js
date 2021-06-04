7c43e611ccf5e01b337bbc15f6a7873b30920253
Sebastian <maxwellsebas@gmail.com>
2021-06-03 20:18:04 -0400

    Created Bot Avatar and did a light style

--- Rappa_Capstone/frontend/src/chatbot/config.js
+++ Rappa_Capstone/frontend/src/chatbot/config.js
@@ -1,7 +1,22 @@
+import React from 'react';
 import { createChatBotMessage } from "react-chatbot-kit";
+import BotAvatar from "./BotAvatar";
 
 const config = {
-  initialMessages: [createChatBotMessage(`Welcome to Rappa Realty Group!`)]
+  initialMessages: [createChatBotMessage(`Welcome to Rappa Realty Group!`)],
+  botName: "Rappa Realty Bot",
+  customComponents: {
+      botAvatar: (props) => <BotAvatar {...props} />
+  },
+  customStyles: {
+      botMessageBox: {
+          backgroundColor: "navy",
+      },
+
+      chatButton: {
+          backgroundColor: "navy",
+      },
+  }
 }
 
 export default config
